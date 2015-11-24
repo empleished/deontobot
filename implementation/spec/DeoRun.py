3f39f36295b535229e58969eae9eb554fc2fb3d1
Leisha Hussien <leishamh@gmail.com>
2015-11-24 01:19:05 +0000

    fixed input type

--- implementation/spec/DeoRun.py
+++ implementation/spec/DeoRun.py
@@ -2,10 +2,17 @@ import antlr3
 from DeoLexer import DeoLexer
 from DeoParser import DeoParser
  
-path_to_input = "/tests/csEthics.txt"
-char_stream = antlr3.ANTLRFileStream(path_to_input)
- 
+path_to_input = "/home/empleished/humeanator/deontobot/implementation/spec/tests/csEthics.txt"
+opened_file = open(path_to_input, 'r')
+input = "";
+
+for line in opened_file: 
+	input = input + line
+
+unicode_input = input.decode("utf-8")
+
+char_stream = antlr3.ANTLRStringStream(unicode_input)
 lexer = DeoLexer(char_stream)
-tokens = antlr3.CommonDeookenStream(lexer)
+tokens = antlr3.CommonTokenStream(lexer)
 parser = DeoParser(tokens)
 parser.entry_rule()
