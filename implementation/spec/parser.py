fb085fe0d18d671432837295e4d9806685624d03
empleished <leishamh@gmail.com>
2016-01-26 20:19:15 +0000

    Update parser.py

--- implementation/spec/parser.py
+++ implementation/spec/parser.py
@@ -2,7 +2,7 @@ import antlr3
 from DeoLexer import DeoLexer
 from DeoParser import DeoParser
 
-char_stream = antlr3.ANTLRFileStream(path_to_input)
+char_stream = antlr3.ANTLRFileStream("tests/csEthics.deo")
 
 lexer = DeoLexer(char_stream)
 tokens = antlr3.CommonTokenStream(lexer)
@@ -10,4 +10,5 @@ parser = DeoParser(tokens)
 rule = parser.prog()
 tree = rule.tree
 
-print tree
+for node in tree: 
+	print node
