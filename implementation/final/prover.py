cf1a3add6697e9db861f2ce65ad409b6ca4bbc94
Leisha Hussien <leishamh@gmail.com>
2016-03-28 04:54:31 +0100

    prover complete

--- implementation/final/prover.py
+++ implementation/final/prover.py
@@ -1,16 +1,16 @@
 import antlr3
-import antlr3.tree
-import antlr3.tokens
 import parser
 
 ''' STEPS '''
 
 steps = []
 
-def addSteps(oldFact, newFact, node, rule): 
+# add prover step to global steps list
+def addSteps(initial, final, transform, rule): 
 	global steps 
-	steps.append([oldFact, newFact, node, rule])
+	steps.append([initial, final, transform, rule])
 
+# process each bit of the prover step for the formatted string
 def processStepBit(bit): 
 	if bit is None:
 		return ""
@@ -25,27 +25,28 @@ def processStepBit(bit):
 	else: 
 		return bit.getText()
 
+# return the list of steps as a formatted string
 def stepsToString(): 
 	global steps
 	count = 0
 	string = ""
 
 	while count < len(steps): 
-		string += "\n\nStep " + str(count + 1) + ": \n"
+		string += "Step " + str(count + 1) + ": \n"
 		initial = steps[count][0]
 		final = steps[count][1]
 		transform = steps[count][2]
 		rule = steps[count][3]
 
-		string += "INITIAL: " + processStepBit(initial) + "\nFINAL: " + processStepBit(final) + "\nTRANSFORM: " + processStepBit(transform) + "\nRULE: " + rule
+		string += "INITIAL: " + processStepBit(initial) + "\nFINAL: " + processStepBit(final) + "\nTRANSFORM: " + processStepBit(transform) + "\nRULE: " + rule + "\n\n"
 		
 		count += 1
 
-	return string[2:]
+	return string
 
 ''' CREATE NEW NODE '''
 
-# stores tokens in a dict
+# stores tokens in a dictionary
 def getAllTokens(): 
 	tokens = {}
 	t = open("Deo.tokens", "r")
@@ -76,6 +77,14 @@ def createNodeWithChildren(tokenText, children):
 	return node
 
 ''' CHECKS '''
+
+# check whether a list l contains an item i 
+def contains(i, l): 
+	for item in l: 
+		if isEqual(i, item): 
+			return True
+
+	return False
 
 # checks whether two nodes are equal
 def isEqual(firstNode, secondNode): 
@@ -144,29 +153,23 @@ def isID(node):
 	return not(isCond(node) or isAnd(node) or isOr(node) or isIOP(node) or isOB(node) or isPRO(node) or isPER(node) or isPOP(node) or isFact(node) or isRule(node) or isGoal(node) or isDecl(node))
 
 def isSingleTerm(node): 
-	return isID(node) or isPOP(node) or isNegation(node)
-
-def contains(i, l): 
-	for item in l: 
-		if isEqual(i, item): 
-			return True
-
-	return False
+	return isID(node) or isPOP(node)
 
 ''' RULES '''
 
 # returns the negation of a fact
 def negation(fact): 
-	if (isNegation(fact.getChild(0))): 
+	if (isNegation(fact.getChild(0))): # create new fact without existing negation node
 		negatedFact = createNodeWithChildren("fact", [fact.getChild(0).getChild(0)])
 	else: 
-		negNode = createNodeWithChildren("not", [fact.getChild(0)])
-		negatedFact = createNodeWithChildren("fact", [negNode])
+		negNode = createNodeWithChildren("not", [fact.getChild(0)]) # create a new negation node for the fact
+		negatedFact = createNodeWithChildren("fact", [negNode]) # create a new fact for the negation node
 
 	return negatedFact
 
 ''' MODUS PONENS - if P and (if P then Q) then Q '''
 
+# recursively check each node of the rule tree to find a match
 def checkModusPonensMatch(facts, node): 
 	if isCond(node): 
 		return checkModusPonensMatch(facts, node)
@@ -180,11 +183,8 @@ def checkModusPonensMatch(facts, node):
 				return True
 		return False
 
+# try and extract a new fact using modus ponens
 def modusPonens(facts, rule): 
-	print "trying modus ponens"
-	steps = []
-	match = False
-
 	match = checkModusPonensMatch(facts, rule.getChild(0))
 
 	if match: 
@@ -196,6 +196,7 @@ def modusPonens(facts, rule):
 
 	return facts
 
+# check that the rule is an if...then expression to try modus ponens
 def tryModusPonens(facts, rule): 
 	if isCond(rule.getChild(0)): 
 		facts = modusPonens(facts, rule.getChild(0))
@@ -204,6 +205,7 @@ def tryModusPonens(facts, rule):
 
 ''' MODUS TOLLENS - if not Q and (if P then Q) then not P '''
 
+# recursively check each node of the rule tree to find a match
 def checkModusTollensMatch(facts, node): 
 	if isCond(node): 
 		return checkModusTollensMatch(facts, node)
@@ -219,11 +221,8 @@ def checkModusTollensMatch(facts, node):
 				return True
 		return False
 
+# try and extract a new fact using modus tollens
 def modusTollens(facts, rule): 
-	print "trying modus tollens"
-	steps = []
-	match = False
-
 	match = checkModusTollensMatch(facts, rule.getChild(1))
 	
 	if match: 
@@ -233,8 +232,10 @@ def modusTollens(facts, rule):
 		if not(contains(new, facts)): 
 			facts.append(new)
 			addSteps(rule.getChild(0), new, rule, "modus tollens")
+	
 	return facts
 
+# check that the rule is an if...then expression to try modus tollens
 def tryModusTollens(facts, rule): 
 	if isCond(rule.getChild(0)): 
 		facts = modusTollens(facts, rule.getChild(0))
@@ -243,78 +244,69 @@ def tryModusTollens(facts, rule):
 	
 ''' DECOMPOSING CONJUNCTION - if (P and Q) then P, Q '''
 
-def decomposeConjunction(node): 
-	print "trying decompose conjunction"
+# recursively check each node of the rule tree to find a match
+def decomposeConjunction(node, facts): 
 	if isAnd(node): 
-		return decomposeConjunction(node.getChild(0)), decomposeConjunction(node.getChild(1))
-	if isNegation(node) or isID(node): 
+		facts = decomposeConjunction(node.getChild(0), facts)
+		facts = decomposeConjunction(node.getChild(1), facts)
+	if isSingleTerm(node): 
 		new = createNodeWithChildren("fact", [node])
-		return new
-	else: 
-		return None
+		if not(contains(new, facts)): 
+			facts.append(new)
+			addSteps(node.parent, new, node.parent, "decompose conjunction")
+			return facts
 
-def processTuple(tuples, factList): 
-	for t in tuples: 
-		if type(t) is tuple: 
-			factList = processTuple(t, factList)
-		else: 
-			factList.append(t)
-	return factList
+	return facts
 
+# try and decompose a conjunction for every fact in the fact set
 def tryDecomposingConjunction(facts): 
 	for fact in facts: 
-		new = decomposeConjunction(fact.getChild(0))
-
-		if type(new) is tuple: 
-			factList = []
-			factList = processTuple(new, factList)
-
-			for f in factList: 
-				if f is not None: 
-					if contains(f, facts) == False: 
-						facts.append(f)
-						addSteps(fact, f, fact, "decompose conjunction")
+		facts = decomposeConjunction(fact.getChild(0), facts)
 
 	return facts
 
 ''' O-NECESSITY '''
 
+# look through fact set for obligations and prohibitions
 def tryONecessity(facts): 
-	print "trying o necessity"
 	for fact in facts: 
 		if isOB(fact.getChild(0)): 
-			ob = createNodeWithChildren("fact", [fact.getChild(0).getChild(0)])
-			newFact = ob
-			if contains(newFact, facts) == False: 
-				facts.append(newFact)
-				addSteps(fact, newFact, fact.getChild(0), "O necessity")
+			ob = createNodeWithChildren("fact", [fact.getChild(0).getChild(0)]) # create a new fact without OB node
+
+			if not(contains(ob, facts)): 
+				facts.append(ob)
+				addSteps(fact, ob, fact.getChild(0), "O necessity")
+
 		elif isPRO(fact.getChild(0)): 
-			neg =  createNodeWithChildren("not", [fact.getChild(0).getChild(0)])
-			pro = createNodeWithChildren("fact", [neg])
-			newFact = pro
-			if contains(newFact, facts) == False: 
-				facts.append(newFact)
-				addSteps(fact, newFact, fact.getChild(0), "O necessity")
+			neg = createNodeWithChildren("not", [fact.getChild(0).getChild(0)]) # create a new negation node
+			pro = createNodeWithChildren("fact", [neg]) # create a new fact for negation node without PRO node 
+
+			if not(contains(pro, facts)): 
+				facts.append(pro)
+				addSteps(fact, pro, fact.getChild(0), "O necessity")
 				
 	return facts
 
 ''' SIMPLIFY FACT SET '''
 
+# go through the fact set and break it down further
 def simplifyFactSet(facts): 
 	progress = True
 
+	# try and decompose facts and find OB/PRO facts
 	while progress: 
 		factSize = len(facts)
 		facts = tryDecomposingConjunction(facts)
 		facts = tryONecessity(facts)
 
-		if len(facts) == factSize:
+		if len(facts) == factSize: # no new facts found
 			progress = False
 
 	return facts
 
 ''' PROOF STRATEGY '''
 
+# check whether each goal is in the fact set
 def isProven(facts, goals):
 	proven = False
 
@@ -322,12 +314,13 @@ def isProven(facts, goals):
 		for fact in facts:
 			if isEqual(fact.getChild(0), goal.getChild(0)):
 				proven = True
-		if proven == False: 
+		if not(proven): 
 			return proven
 
 	return proven
 
-def proofStrategy(goals, facts, rules, count):
+# runs proof strategy with goals, facts and rules
+def proofStrategy(goals, facts, rules):
 	global steps
 	proven = False
 	progress = True
@@ -335,30 +328,23 @@ def proofStrategy(goals, facts, rules, count):
 	# check goal is not already in facts
 	proven = isProven(facts, goals)
 
-	# go through rules
-	while (proven == False and progress == True): 
+	# go through every rule and try and extract more facts
+	while (not(proven) and progress): 
 		factSize = len(facts)
+
 		for rule in rules:
-			print len(facts), "=len facts"
-			print count
-			print "rule:"
-			parser.print_tree(rule, 0)
 			facts = tryModusPonens(facts, rule)
 			facts = tryModusTollens(facts, rule)
+
 		facts = simplifyFactSet(facts)
 
-		if len(facts) == factSize:
+		if len(facts) == factSize: # no new facts found
 			progress = False
 		
 		proven = isProven(facts, goals)
-		count += 1
 
 	# convert steps list to a string
 	proofSteps = stepsToString()
-
-	print "facts:"
-	for fact in facts: 
-		parser.print_tree(fact, 0)
 
 	return proven, proofSteps
 
@@ -370,7 +356,7 @@ def getTerms(tree):
 	for node in tree.getChildren(): 
 		if node.getChild(0).getText() == "TERM": 
 			terms["symbol"] = node.getChild(0).getChild(0).getText()
-			terms["term"] = node.getChild(0).getChild(1).getText()[1:-1]
+			terms["term"] = node.getChild(0).getChild(1).getText()[1:-1] # remove quotes from term
 
 	return terms
 
@@ -401,15 +387,18 @@ def runProver(tree):
 	facts = getFacts(tree)
 	goals = getGoals(tree)
 
-	count = 0
-	proverSteps = proofStrategy(goals, facts, rules, count)
-	print proverSteps[1] # print steps followed by the prover
+	# go through the proof strategy
+	proverSteps = proofStrategy(goals, facts, rules)
+
+	# print steps followed by the prover
+	print proverSteps[1] 
 
 	# print out a statement of success
 	if proverSteps[0] == False:
 		print "STATUS: failure"
 	else:
-		print "\nSTATUS: success"
+		print "STATUS: success"
 
+# get the ANTLR tree from the file parser and run the prover on it
 tree = parser.getTree()
 runProver(tree)
